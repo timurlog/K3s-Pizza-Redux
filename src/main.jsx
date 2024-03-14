ba8788420aac30da881c0916508949a89c335606
timurlog <timur.logie@gmail.com>
2024-03-14 09:13:50 +0100

    init main, added vite react redux tree structure

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+
+export default function Main() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Main />
+  </React.StrictMode>
+);
