19803f8fa49e441d37eb8a937feb4fc757df94f0
timurlog <timur.logie@gmail.com>
2024-03-14 09:17:32 +0100

    added redux folder and code line

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store.js";
 
 export default function Main() {
   const router = createBrowserRouter([
     {
-      path: "/",
-      element: <App />,
+      path: "/K3s-Pizza-Redux/",
+      element: (
+        <Provider store={store}>
+          <App />
+        </Provider>
+      ),
     },
   ]);
 
