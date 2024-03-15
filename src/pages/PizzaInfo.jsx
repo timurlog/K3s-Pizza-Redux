7409e513a0d2317b1c7947eb968223be2f047e24
timurlog <timur.logie@gmail.com>
2024-03-15 09:57:10 +0100

    modified PizzaInfo, init the component

--- src/pages/PizzaInfo.jsx
+++ src/pages/PizzaInfo.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { dataContext } from "../main";
+import { useParams } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 export default function PizzaInfo() {
+  const { data } = useContext(dataContext);
+
+  const { pizzaId } = useParams();
+  const [pizza, setPizza] = useState([]);
+
+  useEffect(() => {
+    setPizza(
+      data.filter((item) =>
+        item.name.toLowerCase().includes(pizzaId.toLowerCase())
+      )
+    );
+  }, [pizzaId, data]);
+
   return (
-    <div>
+    <div className="bg-white h-screen flex flex-col justify-between">
       <Navbar />
-      <div></div>
+      <Footer />
     </div>
   );
 }
