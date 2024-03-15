5126a26fe28c494f3fc324ea705b7c918e76c9f0
timurlog <timur.logie@gmail.com>
2024-03-15 12:13:04 +0100

    added features basket

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import basketSlice from "../features/basket/basket";
+
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    basket: basketSlice,
+  },
 });
